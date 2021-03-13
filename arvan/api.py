7a2b3d7ef3aa9b8d2c32bbec7f66be9544502b93
Sajad <sajadrahimi@aut.ac.ir>
2021-03-13 12:38:39 +0330

    upload file added

--- arvan/api.py
+++ arvan/api.py
@@ -1,4 +1,5 @@
 import os
+from json import JSONDecodeError
 from urllib.parse import urlencode
 
 import requests
@@ -12,25 +13,35 @@ class Arvan:
 		else:
 			self.api_key = os.environ.get('AR_API_KEY')
 
-	def _build_headers(self):
+	def _build_headers(self, headers: dict = None) -> dict:
 		assert self.api_key, 'API KEY must be provided.'
-		return {'AUTHORIZATION': self.api_key}
+		if not headers:
+			headers = {}
+		base_headers = {'AUTHORIZATION': self.api_key}
+		headers = {**headers, **base_headers}
+		return headers
 
 	def _render_response(self, response: Response):
-		if 'data' in response.json():
-			return response.json().get('data')
-		return response.json()
 
-	def _send_request(self, method: str, url: str, data: dict = None, json: dict = None, query_params: dict = None):
+		try:
+			if 'data' in response.json():
+				return response.json().get('data')
+			return response.json()
+		except JSONDecodeError:
+			return response
+
+	def _send_request(self, method: str, url: str, data: dict = None, json: dict = None, query_params: dict = None,
+					  headers: dict = None):
+		headers = self._build_headers(headers)
 		if method.lower() == 'get':
 			if query_params and len(query_params):
 				url = url + '?%s' % (urlencode(query_params))
-			r = requests.get(url, headers=self._build_headers())
+			r = requests.get(url, headers=headers)
 			return self._render_response(r)
 		if method.lower() == 'post':
 			if data and json:
 				raise ValueError('only one of data and json is allowed.')
-			r = requests.post(url, headers=self._build_headers(), data=data, json=json)
+			r = requests.post(url, headers=headers, data=data, json=json)
 			return self._render_response(r)
 
 	def send_request(self, method: str, url: str, data: dict = None, query_params: dict = None):
--- vod/vod.py
+++ vod/vod.py
@@ -1,3 +1,5 @@
+import base64
+import os
 from typing import Union
 
 import requests
@@ -11,6 +13,11 @@ class VOD(Arvan):
 	get_channels_url = 'https://napi.arvancloud.com/vod/2.0/channels'
 	get_videos_urls = 'https://napi.arvancloud.com/vod/2.0/channels/{channel_id}/videos'
 	add_video_url = 'https://napi.arvancloud.com/vod/2.0/channels/{channel}/videos'
+	request_upload_video_url = 'https://napi.arvancloud.com/vod/2.0/channels/{channel}/files'
+	upload_video_url = 'https://napi.arvancloud.com/vod/2.0/channels/{channel}/files/{file}'
+	store_new_video_url = 'https://napi.arvancloud.com/vod/2.0/channels/{channel}/videos'
+	get_file_url = 'https://napi.arvancloud.com/vod/2.0/files/{file}'
+	list_files_url = 'https://napi.arvancloud.com/vod/2.0/channels/{channel}/files'
 
 	def get_channels(self, query_params: dict = None):
 		r = self._send_request('GET', self.get_channels_url, query_params)
@@ -23,7 +30,7 @@ class VOD(Arvan):
 		r = self._send_request('GET', self.get_videos_urls.format(**{'channel_id': channel_id}))
 		return [Video(**x) for x in r]
 
-	def add_video(self, channel: Union[int, Channel], title, video_url,
+	def add_video(self, channel: Union[int, Channel], title: str = None, video_url: str = None, file_id: str = None,
 				  profile_id=None, description=None,
 				  thumbnail_time=None, watermark_id=None, watermark_area=None, convert_info=None,
 				  convert_mode: str = 'auto',
@@ -31,13 +38,58 @@ class VOD(Arvan):
 		channel_id = channel
 		if isinstance(channel, Channel):
 			channel_id = channel.channel_id
+		if bool(file_id) == bool(video_url):
+			raise ValueError('Either file_id or video_url must be provided.')
+		if not title and file_id:
+			title = self.get_file(file_id).get('filename')
+		if file_id:
+			source = {'file_id': file_id}
+		elif video_url:
+			source = {'video_url': video_url}
+		base_data = {
+			"title": title,
+			'description': description,
+			'convert_mode': convert_mode,
+			'parallel_convert': parallel_convert,
+		}
 		url = self.add_video_url.format(**{'channel': channel_id})
-		r = self._send_request('post', url, json={
-							  "title": title,
-							  'description': description,
-							  'video_url': video_url,
-							  'convert_mode': convert_mode,
-							  'parallel_convert': parallel_convert,
-						  })
-		print(r)
+		r = self._send_request('post', url, json={**base_data, **source})
 		return Video(**r)
+
+	def request_upload(self, channel: Union[int, Channel], file_path: str, file_type: str = 'video/mp4',
+					   tus_resumable: str = "1.0.0", upload_length: int = 10096,
+					   upload_metadata: str = None):
+		channel_id = channel
+		if isinstance(channel, Channel):
+			channel_id = channel.channel_id
+		url = self.request_upload_video_url.format(**{'channel': channel_id})
+		file_name = os.path.basename(file_path)
+		size = os.stat(path=file_path).st_size
+		r = self._send_request('post', url,
+							   headers={
+								   'tus-resumable': tus_resumable,
+								   'upload-length': str(size),
+								   'upload-metadata': "filetype %s,filename %s" % (
+									   base64.b64encode(file_type.encode()).decode('ascii'),
+									   base64.b64encode(file_name.encode()).decode('ascii'))
+							   },
+							   )
+		upload_url = r.headers.get('location')
+		if not upload_url:
+			raise AttributeError
+		file_id = upload_url.split('/')[-1]
+		requests.patch(upload_url,
+					   headers=self._build_headers({'content-type': 'application/offset+octet-stream',
+													'upload-offset': '0',
+													'tus-resumable': tus_resumable}),
+					   data=open(file_path, 'rb'))
+		return file_id
+
+	def get_file(self, file_id: str):
+		return self.send_request('get', self.get_file_url.format(**{'file': file_id}))
+
+	def list_files(self, channel: Union[str, Channel]):
+		channel_id = channel
+		if isinstance(channel, Channel):
+			channel_id = channel.channel_id
+		return self.send_request('get', self.list_files_url.format(**{'channel': channel_id}))
