ef049a28ffe7380839ad9a4c23485c8a4d48b563
Sajad <sajadrahimi@aut.ac.ir>
2021-05-04 16:24:55 +0430

    bug fix + build

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ArvanClient",
+    version="0.0.2",
+    author="Sajad Rahimi",
+    author_email="rahimisajad@outlook.com",
+    description="A Python SDK for ArvanCloud API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sajadrahimi/arvan-client",
+    project_urls={
+        "Bug Tracker": "https://github.com/sajadrahimi/arvan-client/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
